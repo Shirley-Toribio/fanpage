ce06ebfa60d0081ee68a651718bbe9a63d507fa4
Shirley Toribio <shirleytoribio@gmail.com>
2018-12-03 21:39:24 +0000

    Completed!

---
+++ oh.js
@@ -0,0 +1,22 @@
+/*global $*/
+
+// DOCUMENT READY FUNCTION BELOW
+$("#o").click(function(){
+    $("#one").css("display","inline");
+    $("#two").css("display","none");
+    $("#three").css("display","none");
+    $(".initial").css("background-image","url('http://www.4usky.com/data/out/40/164343895-green-blue-wallpapers.jpeg')");
+    
+});
+$("#t").click(function(){
+    $("#one").css("display","none");
+    $("#two").css("display","inline");
+    $("#three").css("display","none");
+    $(".initial").css("background-image","url('https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/J9H9WF0/sparkly-white-light-particles-moving-across-a-green-blue-gradient-background_h_opjuy0_thumbnail-full01.png')");
+});
+$("#th").click(function(){
+    $("#one").css("display","none");
+    $("#two").css("display","none");
+    $("#three").css("display","inline");
+    $(".initial").css("background-image","url('https://t4.ftcdn.net/jpg/01/96/35/75/240_F_196357584_p3mTGUQxwtidvPJj5HD8Puvjbf4cQt35.jpg')");
+});
